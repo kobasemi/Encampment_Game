c0ad14745c37515b62d7aba69df85ff70d07dff2
osumi1204 <osumi1204@live.jp>
2015-09-25 18:12:15 +0900

    サーブレット追加

---
+++ game/zintori_ver2/src/zintori_ver2/Data_json.java
@@ -0,0 +1,8 @@
+package zintori_ver2;
+
+public class Data_json {
+	
+	public int data_AI[][][]=new int[502][4][6];
+	public int data_event[][]=new int[502][4];
+	public int data_obstacle[][]=new int[100][2];
+}
